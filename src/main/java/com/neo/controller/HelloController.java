99ae66084f89c11e169fb65c6ab95040f2128aea
jackagoodguy <jak-3@163.com>
2020-12-21 18:14:52 +0800

    dfdfdf

---
+++ src/main/java/com/neo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.neo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class HelloController {
+
+    @Value("${server.servlet.context-path}")
+    private String info;
+	
+    @RequestMapping("/")
+    public String index() {
+       // return "Hello Spring Boot 2.0!";
+        return "this  is "+ info;
+    }
+
+
+    @PostMapping("/postInfo")
+    public String getFile(@RequestParam(value = "key") String key , @RequestParam(value = "value") String value ) {
+
+        return key + " : " + value;
+    }
+}
