2a3fd2b65c846c3628672459201243421147a2ea
jackagoodguy <jak-3@163.com>
2020-12-29 17:36:01 +0800

    add  interceptor

---
+++ src/main/java/com/neo/component/WebmvcConfig.java
@@ -0,0 +1,25 @@
+package com.neo.component;
+
+import com.neo.interceptor.InterceptorDemo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebmvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		super.addInterceptors(registry);
+		registry.addInterceptor(new InterceptorDemo()).addPathPatterns("/**");
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("http://localhost:9992");
+		//registry.addMapping("/api/**").allowedOrigins("http://localhost:8888");
+	}
+
+}
